16065c00d89c8981066a3b77416b973cd67cc04c
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2022-05-02 20:26:30 +0200

    Fixed directory name

--- lab4/ex1/import/index.js
+++
@@ -1,4 +0,0 @@
-import { Operation } from './module.js';
-
-const [x, y] = process.argv.slice(2);
-console.log(new Operation(+x, +y).sum());
--- lab4/ex1/require/index.js
+++
@@ -1,4 +0,0 @@
-const { Operation } = require('./module')
-
-const [x, y] = process.argv.slice(2);
-console.log(new Operation(+x, +y).sum());
